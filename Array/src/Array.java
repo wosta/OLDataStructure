0d6d4d151179eb63d94be3ebd645d03def403f2f
olive <weiwang1202@126.com>
2018-07-26 21:48:26 +0800

    Array add method to add element

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -5,7 +5,7 @@
  **/
 public class Array {
     private int[] data;
-    private int size;
+    private int size;// 当前下标值
 
     public Array(){
         this(10);
@@ -26,6 +26,31 @@ public class Array {
 
     public Boolean isEmpty(){
         return this.size == 0;
+    }
+
+    // 往数组后面添加元素
+    public void addLast(int e){
+        this.add(size, e);
+    }
+
+    // 添加第一个元素
+    public void addFirst(int e){
+        this.add(0, e);
+    }
+
+    public void add(int index, int ele){
+        if (size == this.data.length) {
+            throw new IllegalArgumentException("Add failed, the array is full");
+        }
+        if (index > size || index < 0) {
+            throw new IllegalArgumentException("Add failed. Require index > 0 or index < size");
+        }
+        int i ;
+        for (i = size - 1; i>index; i--){
+            this.data[i] = this.data[i-1];
+        }
+        this.data[index] = ele;
+        size++;
     }
 
     public static void main(String[] args) {
